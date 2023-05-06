61b6f676a0174a5b3a930170e72427c26312e572
Mats <mats@craftmend.com>
2023-05-06 23:06:15 +0200

    Add blob safety

--- src/main/java/com/craftmend/storm/parser/types/objects/adapters/GsonTypeAdapter.java
+++ src/main/java/com/craftmend/storm/parser/types/objects/adapters/GsonTypeAdapter.java
@@ -15,9 +15,20 @@ public class GsonTypeAdapter<T> extends StormTypeAdapter<T> {
     @Override
     public T fromSql(ParsedField parsedField, Object sqlValue) {
         if (sqlValue == null) return null;
-        byte[] bdata = (byte[]) sqlValue;
-        String s = new String(bdata);
-        return parsedField.getStorm().getGson().fromJson(s, TypeToken.get(parsedField.getType()).getType());
+        String json;
+
+        if (sqlValue instanceof Blob) {
+            Blob blob = (Blob) sqlValue;
+            json = new String(blob.getBytes(1, (int) blob.length()), StandardCharsets.UTF_8);
+        } else if (sqlValue instanceof byte[]) {
+            json = new String((byte[]) sqlValue, StandardCharsets.UTF_8);
+        } else if (sqlValue instanceof String) {
+            json = (String) sqlValue;
+        } else {
+            throw new IllegalArgumentException("Cannot convert " + sqlValue.getClass().getName() + " to Gson");
+        }
+
+        return parsedField.getStorm().getGson().fromJson(json, TypeToken.get(parsedField.getType()).getType());
     }
 
     @Override
